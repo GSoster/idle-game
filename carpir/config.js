794a6cb58392318c0612f23ad7e1f1a3312b24f3
GSoster <guilhermesosters@gmail.com>
2017-11-28 17:07:20 -0200

    starting a convention document

--- carpir/config.js
+++ carpir/config.js
@@ -1,6 +1,8 @@
 "use strict";
 
 /**
+ * This file contains configuration information that should be able to be customized 
+ * to allow different templates to work without code rewriting.
  * Fields that start with UI are related to HTML elements.
  */
 
@@ -10,3 +12,12 @@ const __config =
     ui_helpers_list: "ui-helpers-list",//list where the player helpers objects will be shown
 
 };
+
+
+
+
+
+function ApplySettings () 
+{
+
+}
---
+++ carpir/convention.md
@@ -0,0 +1,10 @@
+# HTML  
+**General**  All variables related to HTML elements must be named starting with *ui_*.  
+
+**IDs**  Ids must be named all lowercase and with minus symbol as separator. 
+eg.: ui_helpers_list  
+
+**Class**  Classes must be named all lowercase and with minus symbol as separator. 
+eg.: ui_item_list  
+
+# Javascript
