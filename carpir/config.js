e865f266c6535f8c50893e5b4e2f22ded4370765
GSoster <guilhermesosters@gmail.com>
2017-11-28 16:53:53 -0200

    working on a customizable configuration file

---
+++ carpir/config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * Fields that start with UI are related to HTML elements.
+ */
+
+const __config = 
+{
+    ui_play_area: "ui-play-area",//if of where the player will click to gain resources
+    ui_helpers_list: "ui-helpers-list",//list where the player helpers objects will be shown
+
+};
