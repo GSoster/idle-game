eafcbabc6aae0c46d2e8cc3754785ebe626934e5
GSoster <guilhermesosters@gmail.com>
2017-12-26 09:55:40 -0200

    added two new fields to config: ui_resource_name and ui_max_resource_name

--- carpir/config.js
+++ carpir/config.js
@@ -9,20 +9,25 @@
 
 const __config = 
 {
-    ui_play_area: "ui-play-area",//if of where the player will click to gain resources
+    ui_play_area: "ui-play-area", //if of where the player will click to gain resources
     ui_helpers_list: "ui-helpers-list",//list where the player helpers objects will be shown
-    ui_max_coins: "ui-max-coins",
-    ui_coins: "ui-coins",
+    ui_max_coins: "ui-max-coins", //display the total number of max resource (int value)
+    ui_coins: "ui-coins", //display the current number of resource (int value)
+    ui_resource_name: "ui-resource-name", //display the resource name (eg.: coins)
+    ui_max_resource_name: "ui-max-resource-name", //display the max resource name (eg.: max coins)
 
 };
 
 /**
  * custom config holds information related to the specific game.
  * TODO: This should be moved to a GAME folder or something similar.
+ * Observation: ALL strings here (values) will be capitalized.
  */
 const __custom_config = 
 {
-
+    resource_name: "coins",
+    max_resource_name: "max coins",
+    helpers_list_name: "helpers"
 }
 
 
