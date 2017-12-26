00b50ea460779d2eb51f7e1049de68ff15e9b5ba
GSoster <guilhermesosters@gmail.com>
2017-12-26 10:41:01 -0200

    moving files, restructuring folders

---
+++ carpir/game/custom_settings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * custom settings holds information related to the specific game. 
+ * this configurations should be changed by the developer of the game
+ * TODO: This should be moved to a GAME folder or something similar. 
+ */
+const __custom_settings = 
+{
+    resource_name: "Coins:",
+    max_resource_name: "Max Coins:",
+    helpers_list_name: "Helpers"
+}
