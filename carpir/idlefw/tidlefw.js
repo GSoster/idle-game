235339a638ae26bac16782ccc87e27b3218898e9
GSoster <guilhermesosters@gmail.com>
2017-12-27 20:16:34 -0200

    added temporary css, implemented some logic for event handlers

--- carpir/idlefw/tidlefw.js
+++ carpir/idlefw/tidlefw.js
@@ -1,4 +1,5 @@
 "use strict";
+
 /**
  * Tiny Idle FrameWork
  * 
@@ -12,10 +13,19 @@
 // Declares the logic 
 function OnItemBought(event)
 {
+    //calls the function OnItemBought from the target item.
+    //ex.: bought a helper with ID = 2. Calls the function OnItemBought from helper#2
+}
+
+function OnLevelUp(event)
+{
 
 }
 
-//event handlers
-document.addEventListener('onItemBought', OnItemBought);
-document.addEventListener('', On);
+//General event handlers
+document.addEventListener('OnItemBought', OnItemBought);
+document.addEventListener('OnLevelUp', OnLevelUp);
 
+//Event Handlers for specific areas
+const ui_play_area = document.getElementById('ui-play-area');
+ui_play_area.addEventListener('OnClick', OnClick);
