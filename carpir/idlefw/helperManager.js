e4c786603e7a8f3f4bf565b495aec5ef9e237483
Guilherme Soster <GSoster@users.noreply.github.com>
2018-03-12 17:20:47 -0300

    created a blueprint of resourceManager

--- carpir/idlefw/helperManager.js
+++ carpir/idlefw/helperManager.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var helperManager = (function () {
-
+    'use strict';
     var average = "";
     var media = "";
     return {
---
+++ carpir/idlefw/resourceManager.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var resourceManager = (function () {
+    'use strict';
+    var spendResources = function (quantity){
+
+    }
+
+    return {
+        spendResources: spendResources,
+    }
+})();
