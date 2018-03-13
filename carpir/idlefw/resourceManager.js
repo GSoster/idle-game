a62c5386fa40a4ed50e1f028ca0cc2d7d9af1a9f
Guilherme Soster <GSoster@users.noreply.github.com>
2018-03-13 10:32:03 -0300

    implementation of resourceManager

--- carpir/idlefw/resourceManager.js
+++ carpir/idlefw/resourceManager.js
@@ -1,28 +1,42 @@
 'use strict';
 
-var resourceManager = (function (isDebugEnabled = false) {
+var resourceManager = (function (isDebugEnabled) {
     'use strict';
     var coins = 0;
     var maxCoins = coins;
+    var isDebugEnabled = isDebugEnabled || false;
 
 
     var Produce = function (value)
     {
         coins += value;
         if (coins > maxCoins)
+        {
             maxCoins = coins;
+            if (isDebugEnabled)
+                console.warn(`Debug: resourceManager.Produce called. MaxCoins updated: ${value}`);
+        }
+        if (isDebugEnabled)
+                console.warn(`Debug: resourceManager.Produce called, ${value} produced.`);
         return coins;
     }
 
     var Spend = function (value)
     {
         if (coins > value)
+        {
             coins -= value;
+            if (isDebugEnabled)
+                console.warn(`Debug: resourceManager.Spend called, ${value} spent.`);
+        }      
         return coins;
     }
+
 
     return {
         Produce: Produce,
         Spend: Spend,
+        getCurrentCoins: coins,
+        getMaxCoins: maxCoins
     }
 })(false);
