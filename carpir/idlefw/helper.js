4b29703d1a47b40a220abe6fec223ef70f1c8a08
Guilherme Soster <GSoster@users.noreply.github.com>
2018-06-08 15:36:42 -0300

    improving the helper class

--- carpir/idlefw/helper.js
+++ carpir/idlefw/helper.js
@@ -14,4 +14,38 @@ var Helper = class
         this.id;
     }
 
+    /**
+    * After the framework have checked if the player has enough money to buy the item
+    * the method OnItemBought is called. It them acts as necessary for each item.
+    * The default behavior is  to update its price and play an audio.
+    */
+    OnItemBought ()
+    {            
+        this.buyPrice = this.CalculatePrice();
+        var audio = new Audio("game/assets/sounds/OnItemBought.mp3"); //play audio of being bought
+        audio.play();
+        //TODO: display animation            
+    }
+
+
+    CalculatePrice ()
+    {
+        //global helperManager should be used here        
+        var owned = player.helpers.NumberOfHelpersById(this.id);
+        if (owned === 0)
+            return this.baseCost;
+        var multiplier = 1.09; //TODO: Move this to a global            
+        var price = baseCost * Math.pow(multiplier, owned);
+        return Math.ceil(price);
+    }
+
+    OnItemSold ()
+    {
+        console.error(new Error("Tiny Idle Game Framework: Function not implemented"));
+    }
+    OnLevelUp ()
+    {
+        console.error(new Error("Tiny Idle Game Framework: Function not implemented"));
+    }
+
 }
